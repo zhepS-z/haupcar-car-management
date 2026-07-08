20f57b6e5ba35458c8f0de495e075326b175d8e0
zhepS-z <settapong.janajina@gmail.com>
2026-07-08 21:39:26 +0700

    fix: chnge api url

--- frontend/src/services/carApi.js
+++ frontend/src/services/carApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/cars';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/cars`;
 
 export const getCars = () => axios.get(API_URL);
 export const getCar = (id) => axios.get(`${API_URL}/${id}`);
